2dec7209c0f7009a06e50cb6bb092664a0f62b55
zBabEs <32611645+prapawit201@users.noreply.github.com>
2021-02-11 16:34:04 +0700

    [EDIT] path fot url shortener

--- routes/shortUrl.js
+++ routes/shortUrl.js
@@ -23,14 +23,14 @@ router.post("/link", async (req, res) => {
   if (shortUrl != null) {
     // Created already
     console.log("1");
-    res.status(200).json({ link: "sh.a2.tnpl.me/l/" + shortUrl.short });
+    res.status(200).json({ link: `sh.${process.env.VM_NAME}.tnpl.me/l/` + shortUrl.short });
   } else {
     // Not Create
     // console.log("2");
     const data = await ShortUrl.create({ full: url });
     const { short } = data;
     // console.log(url);
-    res.status(200).json({ link: "sh.a2.tnpl.me/l/" + short });
+    res.status(200).json({ link:  `sh.${process.env.VM_NAME}.tnpl.me/l/` + short });
   }
 });
 
