f44d14badd2c940fdf22a3fe9424270b4d9cb2f0
zBabEs <32611645+prapawit201@users.noreply.github.com>
2021-02-11 12:09:00 +0700

    [Edit] request body full url

--- routes/shortUrl.js
+++ routes/shortUrl.js
@@ -16,15 +16,20 @@ router.get("/l/:shortUrls", async (req, res) => {
 });
 
 router.post("/link", async (req, res) => {
-  const { url } = req.body;
+  const url  = req.body.full;
   const shortUrl = await ShortUrl.findOne({ full: url });
-  if (shortUrl) {
+  // console.log(url);
+  // console.log(shortUrl);
+  if (shortUrl != null) {
     // Created already
+    console.log("1");
     res.status(200).json({ link: "sh.a2.tnpl.me/l/" + shortUrl.short });
   } else {
     // Not Create
+    // console.log("2");
     const data = await ShortUrl.create({ full: url });
     const { short } = data;
+    // console.log(url);
     res.status(200).json({ link: "sh.a2.tnpl.me/l/" + short });
   }
 });
