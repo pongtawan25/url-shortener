e46e035d3f0ad082fea1ed8b63d59673a73e5c2c
Pongtawan Kaenprom <pongtawan2541@gmail.com>
2021-02-23 22:06:10 +0700

    update

--- routes/shortUrl.js
+++ routes/shortUrl.js
@@ -15,30 +15,30 @@ router.get("/l/:shortUrls", async (req, res) => {
 router.post("/link", async (req, res) => {
   const { url } = req.body;
   const shortUrl = await ShortUrl.findOne({ full: url });
-  let checkUrl = url.substring(0, 8);
-  if (!url) {
-    res.status(404).json({
-      status: "Bad Request",
-      message: "Not found any request from user, please try again later",
-    });
-  } else if (checkUrl !== "https://") {
-    res.status(404).json({
-      status: "Bad Request",
-      message: `Full link is incorrect, please start with 'https://' `,
+  // let checkUrl = url.substring(0, 8);
+  // if (!url) {
+  //   res.status(404).json({
+  //     status: "Bad Request",
+  //     message: "Not found any request from user, please try again later",
+  //   });
+  // } else if (checkUrl !== "https://") {
+  //   res.status(404).json({
+  //     status: "Bad Request",
+  //     message: `Full link is incorrect, please start with 'https://' `,
+  //   });
+  // } else {
+  if (shortUrl) {
+    res.status(200).json({
+      link: `http://sh.${process.env.VM_NAME}.tnpl.me/l/${shortUrl._id}`,
     });
   } else {
-    if (shortUrl != null) {
-      res.status(200).json({
-        link: `http://sh.${process.env.VM_NAME}.tnpl.me/l/${shortUrl._id}`,
-      });
-    } else {
-      const data = await ShortUrl.create({ full: url });
-      const { _id } = data;
-      res.status(201).json({
-        link: `http://sh.${process.env.VM_NAME}.tnpl.me/l/${_id}`,
-      });
-    }
+    const data = await ShortUrl.create({ full: url });
+    const { _id } = data;
+    res.status(201).json({
+      link: `http://sh.${process.env.VM_NAME}.tnpl.me/l/${_id}`,
+    });
   }
+  // }
 });
 
 module.exports = router;
