d730e71c701fcf1cf945135ea8d11aef6382aab5
Pongtawan Kaenprom <pongtawan2541@gmail.com>
2021-03-06 15:12:37 +0700

    [UPDATE] route  update stats

--- routes/shortUrl.js
+++ routes/shortUrl.js
@@ -4,26 +4,17 @@ const ShortUrl = require("../models/shortUrl");
 
 router.get("/l/:shortUrls", async (req, res) => {
   const { shortUrls } = req.params;
-
-  // console.log(shortUrls);
   const shortUrl = await ShortUrl.findOne({ link: shortUrls });
-
-  // setTimeout(async function () {
-  await shortUrl.updateOne({ visit: shortUrl.visit + 1 });
-  // console.log("updated");
-  // }, 10000);
-
+  await shortUrl.update({ $inc: { visit: 1 } });
   res.status(302).redirect(shortUrl.url);
 });
 
 router.get("/l/:shortUrls/stats", async (req, res) => {
   const { shortUrls } = req.params;
   const shortUrl = await ShortUrl.findOne({ link: shortUrls });
-
   res.status(200).json({
     visit: shortUrl.visit,
   });
-  console.log("get this path");
 });
 
 router.post("/link", async (req, res) => {
