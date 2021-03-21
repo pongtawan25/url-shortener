7dc415ecca3658402029317a95930fdfb383a2c0
Ubuntu <azureuser@lab2-vm.0tokhvbmsawejlsx4rxzzi2uvd.ix.internal.cloudapp.net>
2021-03-21 09:11:00 +0000

    init commit

--- routes/shortUrl.js
+++ routes/shortUrl.js
@@ -4,14 +4,15 @@ const ShortUrl = require("../models/shortUrl");
 
 router.get("/l/:shortUrls", async (req, res) => {
   const { shortUrls } = req.params;
-  const shortUrl = await ShortUrl.findOne({ link: shortUrls });
-  await shortUrl.update({ $inc: { visit: 1 } });
+  const shortUrl = await ShortUrl.findOneAndUpdate({ link: shortUrls },{ $inc: { visit: 1 } });
+  console.log("Server 1: Redirect");
   res.status(302).redirect(shortUrl.url);
 });
 
 router.get("/l/:shortUrls/stats", async (req, res) => {
   const { shortUrls } = req.params;
   const shortUrl = await ShortUrl.findOne({ link: shortUrls });
+  console.log("Server 1: Get Stat");
   res.status(200).json({
     visit: shortUrl.visit,
   });
@@ -21,15 +22,17 @@ router.post("/link", async (req, res) => {
   const { url } = req.body;
   const shortUrl = await ShortUrl.findOne({ url });
   if (shortUrl) {
-    res.status(200).json({
-      link: `http://sh.${process.env.VM_NAME}.tnpl.me/l/${shortUrl.link}`,
-    });
+  console.log("Server 1: Post");
+   res.status(200).json({
+    link: `http://sh.${process.env.VM_NAME}.tnpl.me/l/${shortUrl.link}`,
+   });
   } else {
-    const data = await ShortUrl.create({ url: url });
-    const { link } = data;
-    res.status(200).json({
-      link: `http://sh.${process.env.VM_NAME}.tnpl.me/l/${link}`,
-    });
+   const data = await ShortUrl.create({ url: url });
+   const { link } = data;
+   console.log("Server 1: Post");
+   res.status(200).json({
+    link: `http://sh.${process.env.VM_NAME}.tnpl.me/l/${link}`,
+   });
   }
 });
 
--- server.js
+++ server.js
@@ -9,15 +9,22 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // const uri = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@cluster0.x39qb.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`;
-const uri = `mongodb://10.1.0.6:27017/urlShortener`;
+const uri = `mongodb://10.0.0.6:27017/urlShortener`;
 mongoose.connect(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
+  poolSize: 200,
+  useCreateIndex: true,
+  useFindAndModify: false,
+  autoIndex: true,
+  serverSelectionTimeoutMS: 5000,
+  socketTimeoutMS: 45000,
+  family: 4
 });
 
 app.use(routeShortUrl);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log("Server is starting port : " + PORT);
+  console.log("Server 1 is starting port : " + PORT);
 });
