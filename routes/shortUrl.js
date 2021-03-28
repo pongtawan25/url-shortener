d2402b88dbf92a54876d373b6e326011ee1704dd
Pongtawan Kaenprom <pongtawan2541@gmail.com>
2021-03-28 11:49:48 +0700

    [EDIT] post route

--- routes/shortUrl.js
+++ routes/shortUrl.js
@@ -4,15 +4,18 @@ const ShortUrl = require("../models/shortUrl");
 
 router.get("/l/:shortUrls", async (req, res) => {
   const { shortUrls } = req.params;
-  const shortUrl = await ShortUrl.findOneAndUpdate({ link: shortUrls },{ $inc: { visit: 1 } });
-  console.log("Server 1: Redirect");
+  const shortUrl = await ShortUrl.findOneAndUpdate(
+    { link: shortUrls },
+    { $inc: { visit: 1 } }
+  );
+  console.log("Server: Redirect");
   res.status(302).redirect(shortUrl.url);
 });
 
 router.get("/l/:shortUrls/stats", async (req, res) => {
   const { shortUrls } = req.params;
   const shortUrl = await ShortUrl.findOne({ link: shortUrls });
-  console.log("Server 1: Get Stat");
+  console.log("Server: Get Stat");
   res.status(200).json({
     visit: shortUrl.visit,
   });
@@ -20,20 +23,18 @@ router.get("/l/:shortUrls/stats", async (req, res) => {
 
 router.post("/link", async (req, res) => {
   const { url } = req.body;
-  // const shortUrl = await ShortUrl.findOne({ url });
-  // if (shortUrl) {
-  // console.log("Server 1: Post");
-  //  res.status(200).json({
-  //   link: `http://sh.${process.env.VM_NAME}.tnpl.me/l/${shortUrl.link}`,
-  //  });
-  // } else {
-   const data = await ShortUrl.create({ url: url });
-   const { link } = data;
-   console.log("Server 1: Post");
-   res.status(200).json({
-    link: `http://sh.${process.env.VM_NAME}.tnpl.me/l/${link}`,
-   });
-  // }
+  const shortUrl = await ShortUrl.findOne({ url: url });
+  if (!shortUrl) {
+    const data = await ShortUrl.create({ url: url });
+    const { link } = data;
+    console.log("Server: Post");
+    res.status(200).json({
+      link: `http://sh.${process.env.VM_NAME}.tnpl.me/l/${link}`,
+    });
+  }
+  res.status(200).json({
+    link: `http://sh.${process.env.VM_NAME}.tnpl.me/l/${shortUrl.link}`,
+  });
 });
 
 module.exports = router;
