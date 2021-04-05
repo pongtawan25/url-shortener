5e1b440ca4eab1463a1acb5f8bb06d93fb34d068
zBabEs <32611645+prapawit201@users.noreply.github.com>
2021-04-05 22:12:51 +0700

    [UPDATE] update post url

--- server.js
+++ server.js
@@ -10,8 +10,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cors());
 
-// const uri = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@cluster0.x39qb.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`;
-const uri = `mongodb://${process.env.DB_IP}`;
+const uri = `mongodb://${process.env.DB_IP}/?maxPoolSize=500&poolSize=400`;
 MongoClient.connect(uri, (error, client) => {
   if (error) throw error;
   var db = client.db("urlShortener");
@@ -20,35 +19,29 @@ MongoClient.connect(uri, (error, client) => {
     res.send("Hello World, Url-shortener Test MongoDB");
   });
   app.post("/link", async (req, res) => {
-    console.log('check in method');
     const { url } = req.body;
-    //console.log(url);
-      console.log("Server: Post - 1");
-      let gen_id = nanoid(3);
-      console.log(gen_id);
-      const data = await db.collection("shorturls").insertOne({
-        url: url,
-        link: gen_id,
-        visit: 0 
-      })
-      console.log(...data.ops);
-      res.status(200).json({
-        link: `http://localhost:5000/l/${gen_id}`,//sh.${process.env.VM_NAME}.tnpl.me
-      });
-    
+    let gen_id = nanoid(3);
+    await db.collection("shorturls").insertOne({
+      url: url,
+      link: gen_id,
+      visit: 0,
+    });
+    res.status(200).json({
+      link: `http://sh.${process.env.VM_NAME}.tnpl.me/l/${gen_id}`, //sh.${process.env.VM_NAME}.tnpl.me
+    });
   });
   app.get("/l/:shortUrls", async (req, res) => {
     const { shortUrls } = req.params;
-    // console.log(shortUrls);
-    const result = await db.collection('shorturls').findOneAndUpdate(
-      {link: shortUrls},
-      {$inc: {visit:1}}
-    )
-     return res.status(302).redirect(result.value.url);
+    const result = await db
+      .collection("shorturls")
+      .findOneAndUpdate({ link: shortUrls }, { $inc: { visit: 1 } });
+    return res.status(302).redirect(result.value.url);
   });
   app.get("/l/:shortUrls/stats", async (req, res) => {
     const { shortUrls } = req.params;
-    const shortUrl = await db.collection('shorturls').findOne({ link: shortUrls });
+    const shortUrl = await db
+      .collection("shorturls")
+      .findOne({ link: shortUrls });
     res.status(200).json({
       visit: shortUrl.visit,
     });
