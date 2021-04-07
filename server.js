e0baa6891286a321d4b59897045e23c5a0d5ca1c
Pongtawan Kaenprom <32630484+pongtawan25@users.noreply.github.com>
2021-04-07 19:59:55 +0700

    [EDIT] post route

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ MongoClient.connect(uri, { useUnifiedTopology: true }, (error, client) => {
   });
   app.post("/link", async (req, res) => {
     const { url } = req.body;
-    let gen_id = nanoid(3);
+    let gen_id = nanoid(6);
     try {
       await db.collection("shorturls").insertOne({
         url: url,
