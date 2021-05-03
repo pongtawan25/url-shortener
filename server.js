f2a37deb77f18579d840a1ed9f83fe6b9196913c
zBabEs <32611645+prapawit201@users.noreply.github.com>
2021-05-03 15:13:20 +0700

    [UPDATE] update poolsize

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-const uri = `mongodb://${process.env.DB_IP}/urlShortener/?poolSize=400`;
+const uri = `mongodb://${process.env.DB_IP}/urlShortener/?poolSize=500`;
 MongoClient.connect(uri, { useUnifiedTopology: true }, (error, client) => {
   if (error) throw error;
   var db = client.db("urlShortener");
