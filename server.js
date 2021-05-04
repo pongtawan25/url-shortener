3f8dc1d9b60f9656b6254713c4ba7309f1256ee7
zBabEs <32611645+prapawit201@users.noreply.github.com>
2021-05-04 14:28:01 +0700

    initial commit

--- server.js
+++ server.js
@@ -7,30 +7,35 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-
-const uri = `mongodb://${process.env.DB_IP}/?poolSize=450`;
+const uri = `mongodb://${process.env.DB_IP}/?maxPoolSize=500&poolSize=450`;
 MongoClient.connect(uri, { useUnifiedTopology: true }, (error, client) => {
   if (error) throw error;
   var db = client.db("urlShortener");
 
   app.get("/", (req, res) => {
-    res.send("Welcome to Url-Shortener API service");
+    res.send("Hello World, Url-shortener Test MongoDB");
   });
 
   app.post("/link", async (req, res) => {
     const { url } = req.body;
     let gen_id = nanoid(6);
-        await db.collection("shorturls").insertOne(
-        {
-          url: url,
-          link: gen_id,
-          visit: 0,
-        },
-      );
-      res.status(200).json({
-        link: `http://sh.a2.tnpl.me/l/${gen_id}`,
+    try {
+      await db.collection("shorturls").insertOne({
+        url: url,
+        link: gen_id,
+        visit: 0,
       });
- 
+      res.status(200).json({
+        link: `http://sh.${process.env.VM_NAME}.tnpl.me/l/${gen_id}`,
+      });
+    } catch (error) {
+      const data = await db.collection("shorturls").findOne({
+        url: url,
+      });
+      res.status(200).json({
+        link: `http://sh.${process.env.VM_NAME}.tnpl.me/l/${data.link}`,
+      });
+    }
   });
 
   app.get("/l/:shortUrls", async (req, res) => {
@@ -50,9 +55,9 @@ MongoClient.connect(uri, { useUnifiedTopology: true }, (error, client) => {
       visit: shortUrl.visit,
     });
   });
-});
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log("server started : " + PORT);
+  const PORT = process.env.PORT || 5000;
+  app.listen(PORT, () => {
+    console.log("server started : " + PORT);
+  });
 });
