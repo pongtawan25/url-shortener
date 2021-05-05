ca14275c1461662794f37fdbe50e6c64139f8691
zBabEs <32611645+prapawit201@users.noreply.github.com>
2021-05-05 20:26:23 +0700

    esitt max connection

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-const uri = `mongodb://${process.env.DB_IP}/?maxPoolSize=700`;
+const uri = `mongodb://${process.env.DB_IP}/?maxPoolSize=900`;
 MongoClient.connect(uri, { useUnifiedTopology: true }, (error, client) => {
   if (error) throw error;
   var db = client.db("urlShortener");
