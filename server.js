6a9c7e88677ab71c04c60bd8702f675c77449408
Pongtawan Kaenprom <32630484+pongtawan25@users.noreply.github.com>
2021-05-06 16:12:30 +0700

    [EDIT] maxPoolSize

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-const uri = `mongodb://${process.env.DB_IP}/?maxPoolSize=500&poolSize=450`;
+const uri = `mongodb://${process.env.DB_IP}/?maxPoolSize=200`;
 MongoClient.connect(uri, { useUnifiedTopology: true }, (error, client) => {
   if (error) throw error;
   var db = client.db("urlShortener");
